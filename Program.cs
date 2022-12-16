befbce213d12021d5959eafc4bc483043f1223ee
Paweł Bronisław Margul <margulpawel@gmail.com>
2022-12-16 12:56:55 +0100

    Add support to XML response format

--- Program.cs
+++ Program.cs
@@ -3,20 +3,20 @@ using MilleniumAspWebAPI.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false)
     .Build();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddXmlSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<BooksDbContext>(
         options => options.UseSqlServer(config.GetConnectionString("BookDb")));
-
 
 builder.Services.AddScoped<IBookDbService, BookDbService>();
 
